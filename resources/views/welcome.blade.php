c37d1fc904ff5754e4b9846a542cd5371703aed3
Philip Filippenko <philip.filippenko@students.ittralee.ie>
2025-11-11 06:35:04 +0000

    Welcome Back:
    FIxed logo issue
    Applied new design
    Removed unused welcome.css file
    Removed unused php-bank-logo.svg file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,12 +7,13 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="auth-page">
-    <div class="text-center max-w-2xl px-8">
-        <img src="{{ asset('images/php-bank-logo.svg') }}" alt="PHP BANK" class="w-36 mx-auto mb-16">
-        <h1 class="text-5xl md:text-6xl font-light mb-12 tracking-tight">Welcome Back</h1>
+    <div class="text-center max-w-2xl px-8 -mt-20">
+        <img src="{{ asset('images\Design Images\Logo\PHP BANK Logo.svg') }}" alt="PHP BANK" class="w-36 mx-auto mb-5">
+        <h1 class="text-5xl lg:text-6xl font-bold mb-12 tracking-tight">Welcome Back</h1>
+
 
         <div class="flex flex-col sm:flex-row gap-6 justify-center mt-12">
-            <a href="{{ route('login') }}" class="btn-primary">Sign In</a>
+            <a href="{{ route('login') }}" class="btn-primary">Log In</a>
             <a href="{{ route('register') }}" class="btn-secondary">Create Account</a>
         </div>
     </div>
